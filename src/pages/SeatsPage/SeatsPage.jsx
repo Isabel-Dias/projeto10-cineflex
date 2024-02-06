e50a4f36a76fac73d767e77bac54a17f3979c955
Isabel-Dias <isabel.c.andrade.dias@gmail.com>
2024-02-06 16:17:48 -0300

    fix: indentation and function sizes adjusted to fit conventions

--- src/pages/SeatsPage/SeatsPage.jsx
+++ src/pages/SeatsPage/SeatsPage.jsx
@@ -1,243 +1,288 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { Form, Link, useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
 export default function SeatsPage(props) {
-    const [seats, setSeats] = useState([]);
-    const params = useParams();
-    const { idSessao } = params;
-    const navigate = useNavigate();
-    const {selectedSeats, setSelectedSeats, seatNames, setSeatNames, name, setName, cpf, setCpf, setMovieName, setSessionDate, setSessionTime} = props;
+  const [seats, setSeats] = useState([]);
+  const params = useParams();
+  const { idSessao } = params;
+  const navigate = useNavigate();
+  const {
+    selectedSeats,
+    setSelectedSeats,
+    seatNames,
+    setSeatNames,
+    name,
+    setName,
+    cpf,
+    setCpf,
+    setMovieName,
+    setSessionDate,
+    setSessionTime,
+  } = props;
 
+  function setSeats(selectedSeatID, selectedSeatName) {
+    newSeats = [...selectedSeats, selectedSeatID];
+    setSelectedSeats(newSeats);
+    setSeatNames([...seatNames, selectedSeatName]);
+    window.localStorage.setItem(
+      "Stored_Selected_Seats",
+      JSON.stringify(selectedSeats)
+    );
+  }
 
-    function selectSeats(selectedSeatID, selectedSeatName) {
-        const data = window.localStorage.getItem('Stored_Selected_Seats');
-        if ( data !== null ) setSelectedSeats(JSON.parse(data));
-        let newSeats = [];
+  function defineSeatsStyling(seat, selectedSeatID) {
+    if (seat.id == selectedSeatID && seat.isAvailable == true) {
+      seat.isAvailable = "selected";
+    } else if (seat.id == selectedSeatID && seat.isAvailable == "selected") {
+      seat.isAvailable = true;
+      newSeats = [...selectedSeats];
+      newSeats.splice(seat.id);
+      setSelectedSeats(newSeats);
+    }
+  }
 
-        if (!selectedSeats.includes(selectedSeatID)) {
-            newSeats = [...selectedSeats, selectedSeatID]
-            setSelectedSeats(newSeats);
-            setSeatNames([...seatNames, selectedSeatName]);
-            window.localStorage.setItem('Stored_Selected_Seats', JSON.stringify(selectedSeats));
-        }
+  function selectSeats(selectedSeatID, selectedSeatName) {
+    const data = window.localStorage.getItem("Stored_Selected_Seats");
+    if (data !== null) setSelectedSeats(JSON.parse(data));
+    let newSeats = [];
 
-        seats.seats.forEach(seat => {
-            if (seat.id == selectedSeatID && seat.isAvailable == true) {
-                seat.isAvailable = 'selected'
-            } else if (seat.id == selectedSeatID && seat.isAvailable == 'selected') {
-                seat.isAvailable = true;
-                newSeats = [...selectedSeats]
-                newSeats.splice(seat.id);
-                setSelectedSeats(newSeats);
-            } 
-        })
+    if (!selectedSeats.includes(selectedSeatID)) {
+      setSeats(selectedSeatID, selectedSeatName);
     }
 
-    function reserveSeats(event) {
-        event.preventDefault();
-        
-        const reservationInfo = {
-            ids: selectedSeats,
-            name: name,
-            cpf: cpf
-        }
+    seats.seats.forEach((seat) => {
+      defineSeatsStyling(seat, selectedSeatID);
+    });
+  }
 
+  function reserveSeats(event) {
+    event.preventDefault();
 
-        const promise = axios.post(`${import.meta.env.VITE_API_URL}seats/book-many`, reservationInfo, config)
-        promise.then(p => {
-            setMovieName(seats.movie.title); 
-            setSessionDate(seats.day.date);
-            setSessionTime(seats.name);
-            navigate('/sucesso')})
-        promise.catch(p => console.log(p.response))
-    }
+    const reservationInfo = {
+      ids: selectedSeats,
+      name: name,
+      cpf: cpf,
+    };
 
-    const config = {
-        headers: {
-            "Authorization": "L1tQ0XU9l1CXlCYOvMtwa96V"
-        }
-    }
-    useEffect(() => {
-        const promise = axios.get(`${import.meta.env.VITE_API_URL}${idSessao}/seats`, config);
+    const promise = axios.post(
+      `${import.meta.env.VITE_API_URL}seats/book-many`,
+      reservationInfo,
+      config
+    );
+    promise.then((p) => {
+      setMovieName(seats.movie.title);
+      setSessionDate(seats.day.date);
+      setSessionTime(seats.name);
+      navigate("/sucesso");
+    });
+    promise.catch((p) => console.log(p.response));
+  }
 
-        promise.then(p => {
-            setSeats(p.data)
-        });
+  const config = {
+    headers: {
+      Authorization: "L1tQ0XU9l1CXlCYOvMtwa96V",
+    },
+  };
+  useEffect(() => {
+    const promise = axios.get(
+      `${import.meta.env.VITE_API_URL}${idSessao}/seats`,
+      config
+    );
 
-    }, []);
+    promise.then((p) => {
+      setSeats(p.data);
+    });
+  }, []);
 
-
-
-    return (
-        <PageContainer>
-            Selecione o(s) assento(s)
-            <SeatsContainer>
-                {seats?.seats?.map(seat => (
-                    <SeatItem key={seat.id} onClick={() => { selectSeats(seat.id, seat.name)}}
-                        color={seat.isAvailable == true ? "#C3CFD9" : seat.isAvailable == false ? "#FBE192" : "#1AAE9E"}
-                        border={seat.isAvailable == true ? "#808F9D" : seat.isAvailable == false ? "#F7C52B" : "#0E7D71"}
-                    >
-                        {seat.name}
-                    </SeatItem>
-                ))}
-            </SeatsContainer>
-
-            <CaptionContainer>
-                <CaptionItem>
-                    <CaptionCircle color="#1AAE9E" border="#0E7D71" />
-                    Selecionado
-                </CaptionItem>
-                <CaptionItem>
-                    <CaptionCircle color="#C3CFD9" border="#808F9D" />
-                    Disponível
-                </CaptionItem>
-                <CaptionItem>
-                    <CaptionCircle color="#FBE192" border="#F7C52B" />
-                    Indisponível
-                </CaptionItem>
-            </CaptionContainer>
-            <form onSubmit={reserveSeats}>
-                <FormContainer>
-
-                    Nome do Comprador:
-                    <input onChange={e => setName(e.target.value)} required placeholder="Digite seu nome..." />
-
-                    CPF do Comprador:
-                    <input onChange={e => setCpf(e.target.value)} type="number" required placeholder="Digite seu CPF..." />
-                    
-                    <button type="submit ">Reservar Assento(s)</button>
-                </FormContainer>
-            </form>
-
-
-            <FooterContainer>
-                <div>
-                    <img src={seats?.movie?.posterURL} />
-                </div>
-                <div>
-                    <p>{seats?.movie?.title}</p>
-                    <p>{seats?.day?.weekday} - {seats?.day?.date}</p>
-                </div>
-            </FooterContainer>
-
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      Selecione o(s) assento(s)
+      <SeatsContainer>
+        {seats?.seats?.map((seat) => (
+          <SeatItem
+            key={seat.id}
+            onClick={() => {
+              selectSeats(seat.id, seat.name);
+            }}
+            color={
+              seat.isAvailable == true
+                ? "#C3CFD9"
+                : seat.isAvailable == false
+                ? "#FBE192"
+                : "#1AAE9E"
+            }
+            border={
+              seat.isAvailable == true
+                ? "#808F9D"
+                : seat.isAvailable == false
+                ? "#F7C52B"
+                : "#0E7D71"
+            }
+          >
+            {seat.name}
+          </SeatItem>
+        ))}
+      </SeatsContainer>
+      <CaptionContainer>
+        <CaptionItem>
+          <CaptionCircle color="#1AAE9E" border="#0E7D71" />
+          Selecionado
+        </CaptionItem>
+        <CaptionItem>
+          <CaptionCircle color="#C3CFD9" border="#808F9D" />
+          Disponível
+        </CaptionItem>
+        <CaptionItem>
+          <CaptionCircle color="#FBE192" border="#F7C52B" />
+          Indisponível
+        </CaptionItem>
+      </CaptionContainer>
+      <form onSubmit={reserveSeats}>
+        <FormContainer>
+          Nome do Comprador:
+          <input
+            onChange={(e) => setName(e.target.value)}
+            required
+            placeholder="Digite seu nome..."
+          />
+          CPF do Comprador:
+          <input
+            onChange={(e) => setCpf(e.target.value)}
+            type="number"
+            required
+            placeholder="Digite seu CPF..."
+          />
+          <button type="submit ">Reservar Assento(s)</button>
+        </FormContainer>
+      </form>
+      <FooterContainer>
+        <div>
+          <img src={seats?.movie?.posterURL} />
+        </div>
+        <div>
+          <p>{seats?.movie?.title}</p>
+          <p>
+            {seats?.day?.weekday} - {seats?.day?.date}
+          </p>
+        </div>
+      </FooterContainer>
+    </PageContainer>
+  );
 }
 
 const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: 'Roboto';
-    font-size: 24px;
-    text-align: center;
-    color: #293845;
-    margin-top: 30px;
-    padding-bottom: 120px;
-    padding-top: 70px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Roboto";
+  font-size: 24px;
+  text-align: center;
+  color: #293845;
+  margin-top: 30px;
+  padding-bottom: 120px;
+  padding-top: 70px;
+`;
 const SeatsContainer = styled.div`
-    width: 330px;
+  width: 330px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
+const FormContainer = styled.div`
+  width: calc(100vw - 40px);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 20px 0;
+  font-size: 18px;
+  text-decoration: none;
+  button {
+    align-self: center;
+  }
+  input {
+    width: calc(100vw - 60px);
+  }
+`;
+const CaptionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 300px;
+  justify-content: space-between;
+  margin: 20px;
+`;
+const CaptionCircle = styled.div`
+  border: 1px solid ${(props) => props.border};
+  background-color: ${(props) => props.color};
+  height: 25px;
+  width: 25px;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5px 3px;
+`;
+const CaptionItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 12px;
+`;
+const SeatItem = styled.div`
+  border: 1px solid ${(props) => props.border};
+  background-color: ${(props) => props.color};
+  height: 25px;
+  width: 25px;
+  border-radius: 25px;
+  font-family: "Roboto";
+  font-size: 11px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5px 3px;
+  &:hover {
+    cursor: ${(props) => (props.color == "#FBE192" ? "default" : "pointer")};
+  }
+`;
+const FooterContainer = styled.div`
+  width: 100%;
+  height: 120px;
+  background-color: #c3cfd9;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 20px;
+  position: fixed;
+  bottom: 0;
+
+  div:nth-child(1) {
+    box-shadow: 0px 2px 4px 2px #0000001a;
+    border-radius: 3px;
     display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
     align-items: center;
     justify-content: center;
-    margin-top: 20px;
-`
-const FormContainer = styled.div`
-    width: calc(100vw - 40px); 
+    background-color: white;
+    margin: 12px;
+    img {
+      width: 50px;
+      height: 70px;
+      padding: 8px;
+    }
+  }
+
+  div:nth-child(2) {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    margin: 20px 0;
-    font-size: 18px;
-    text-decoration: none;
-    button {
-        align-self: center;
+    p {
+      text-align: left;
+      &:nth-child(2) {
+        margin-top: 10px;
+      }
     }
-    input {
-        width: calc(100vw - 60px);
-    }
-`
-const CaptionContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 300px;
-    justify-content: space-between;
-    margin: 20px;
-`
-const CaptionCircle = styled.div`
-    border: 1px solid ${props => props.border};         // Essa cor deve mudar
-    background-color: ${props => props.color};    // Essa cor deve mudar
-    height: 25px;
-    width: 25px;
-    border-radius: 25px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 5px 3px;
-`
-const CaptionItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-size: 12px;
-`
-const SeatItem = styled.div`
-    border: 1px solid ${props => props.border};        // Essa cor deve mudar
-    background-color: ${props => props.color};    // Essa cor deve mudar
-    height: 25px;
-    width: 25px;
-    border-radius: 25px;
-    font-family: 'Roboto';
-    font-size: 11px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 5px 3px;
-    &:hover {
-        cursor: ${props => props.color == "#FBE192" ? 'default' : 'pointer'}
-    }
-`
-const FooterContainer = styled.div`
-    width: 100%;
-    height: 120px;
-    background-color: #C3CFD9;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    font-size: 20px;
-    position: fixed;
-    bottom: 0;
-
-    div:nth-child(1) {
-        box-shadow: 0px 2px 4px 2px #0000001A;
-        border-radius: 3px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: white;
-        margin: 12px;
-        img {
-            width: 50px;
-            height: 70px;
-            padding: 8px;
-        }
-    }
-
-    div:nth-child(2) {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        p {
-            text-align: left;
-            &:nth-child(2) {
-                margin-top: 10px;
-            }
-        }
-    }
-`
+  }
+`;
--- src/pages/SuccessPage/SuccessPage.jsx
+++ src/pages/SuccessPage/SuccessPage.jsx
@@ -1,73 +1,84 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export default function SuccessPage(props) {
-    const { name, cpf, movieName, sessionDate, sessionTime, seatNames } = props;
+  const { name, cpf, movieName, sessionDate, sessionTime, seatNames } = props;
 
-    return (
-        <PageContainer>
-            <h1>Pedido feito <br /> com sucesso!</h1>
+  return (
+    <PageContainer>
+      <h1>
+        Pedido feito <br /> com sucesso!
+      </h1>
 
-            <TextContainer>
-                <strong><p>Filme e sessão</p></strong>
-                <p>{movieName}</p>
-                <p>{sessionDate} - {sessionTime}</p>
-            </TextContainer>
+      <TextContainer>
+        <strong>
+          <p>Filme e sessão</p>
+        </strong>
+        <p>{movieName}</p>
+        <p>
+          {sessionDate} - {sessionTime}
+        </p>
+      </TextContainer>
 
-            <TextContainer>
-                <strong><p>Ingressos</p></strong>
-                {seatNames.map(seat => <p> Assento {seat}</p>)}
-            </TextContainer>
+      <TextContainer>
+        <strong>
+          <p>Ingressos</p>
+        </strong>
+        {seatNames.map((seat) => (
+          <p> Assento {seat}</p>
+        ))}
+      </TextContainer>
 
-            <TextContainer>
-                <strong><p>Comprador</p></strong>
-                <p>Nome: {name}</p>
-                <p>CPF: {cpf}</p>
-            </TextContainer>
-            <Link to="/">
-                <button>Voltar para Home</button>
-            </Link>
-        </PageContainer>
-    )
+      <TextContainer>
+        <strong>
+          <p>Comprador</p>
+        </strong>
+        <p>Nome: {name}</p>
+        <p>CPF: {cpf}</p>
+      </TextContainer>
+      <Link to="/">
+        <button>Voltar para Home</button>
+      </Link>
+    </PageContainer>
+  );
 }
 
 const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: 'Roboto';
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Roboto";
+  font-size: 24px;
+  color: #293845;
+  margin: 30px 20px;
+  padding-bottom: 120px;
+  padding-top: 70px;
+  a {
+    text-decoration: none;
+  }
+  button {
+    margin-top: 50px;
+  }
+  h1 {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
     font-size: 24px;
-    color: #293845;
-    margin: 30px 20px;
-    padding-bottom: 120px;
-    padding-top: 70px;
-    a {
-        text-decoration: none;
-    }
-    button {
-        margin-top: 50px;
-    }
-    h1 {
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 24px;
-        line-height: 28px;
-        display: flex;
-        align-items: center;
-        text-align: center;
-        color: #247A6B;
-    }
-`
-const TextContainer = styled.div`
-    width: 100%;
+    line-height: 28px;
     display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin-top: 30px;
-    strong {
-        font-weight: bold;
-        margin-bottom: 10px;
-    }
-    
-`
+    align-items: center;
+    text-align: center;
+    color: #247a6b;
+  }
+`;
+const TextContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 30px;
+  strong {
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+`;
